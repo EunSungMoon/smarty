c62b6d76bf7293b3c0cca9d14b5958e5a5948740
Eunsung Moon <eunsung9647@gmail.com>
2022-02-24 23:14:44 +0900

    footer

--- src/Components/layout/Footer.tsx
+++ src/Components/layout/Footer.tsx
@@ -1,5 +1,10 @@
 export default function Footer() {
-  return(
-    <div>footer</div>
+  return (
+    <footer id='footer'>
+      <div className="footerInner container">
+        <p>Copyright 2022. friends all rights reserved.</p>
+        <p>FriendsForKorea@gmail.com</p>
+      </div>
+    </footer>
   )
 }
